630728f8368ca6030f9beb5b956d3e2d35dd7be0
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-05 03:11:56 +0200

    metadata

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.30j80sgekfg"
+    "revision": "0.3rt4c63bms"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
