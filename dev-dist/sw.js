10513fcb2995081fa2890791d8eb290dcd7d94f6
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-05 05:21:33 +0200

    from js to ts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-c6c6fb7c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v2mcesuncng"
+    "revision": "0.bl200mtq0gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
